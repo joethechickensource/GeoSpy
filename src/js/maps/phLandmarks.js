36644da354ed530e3553b800d97abf1a70e0fa4c
Jizan Diaz Villafuerte <diazjizan@gmail.com>
2023-11-22 13:38:20 +0800

    Add ph category

---
+++ src/js/maps/phLandmarks.js
@@ -0,0 +1,222 @@
+let panorama;
+var map;
+var overlay = document.getElementById("overlay");
+var ui = document.getElementById("ui");
+var guess = document.getElementById("guess");
+var guessCont = document.getElementById("button-wrapper");
+var guessbtn = document.getElementById("guess-btn");
+var completeDiv = document.getElementById("completeLevel");
+var next = document.getElementById("next");
+var tools = document.getElementById("tools");
+var playerScore = 0;
+var distanceScore = 5000;
+var currentCordsLat, currentCordsLng;
+var currentLoc, currentCoords;
+function phMap(n) {
+  var location = [
+    [{ lat: 9.829708, lng: 124.1396756 }, { city: "Chocolate Hills" }],
+    [{ lat: 16.9239126, lng: 121.055836 }, { city: "Rice Terraces" }],
+    [{ lat: 14.0178494, lng: 120.9970307 }, { city: "Taal Lake" }],
+    [{ lat: 11.17217, lng: 125.0121195 }, { city: "Leyte Landing Monument" }],
+    [{ lat: 14.4451027, lng: 120.9069061 }, { city: "Aguinaldo Shrine" }],
+    [{ lat: 14.3895652, lng: 120.5991966 }, { city: "Corregidor Island" }],
+    [{ lat: 14.5941553, lng: 120.9705171 }, { city: "Fort Santiago" }],
+    [{ lat: 14.5816644, lng: 120.9768291 }, { city: "Luneta Park" }],
+    [{ lat: 14.5943746, lng: 120.9944327 }, { city: "Malacañan Palace" }],
+  ];
+
+  currentLoc = location[n];
+  currentCoords = currentLoc[0];
+  return currentCoords;
+}
+function levelComplete() {
+  guess.style.height = "100vh";
+  guess.style.width = "100vw";
+  guess.style.margin = "0";
+  tools.style.display = "none";
+  guessbtn.style.display = "none";
+  completeDiv.style.display = "flex";
+}
+function checkDistance() {
+  var lat1 = guessMarker.position.lat();
+  var lng1 = guessMarker.position.lng();
+  var lat2 = currentCordsLat;
+  var lng2 = currentCordsLng;
+  var R = 6371;
+  var dLat = deg2rad(lat2 - lat1);
+  var dLng = deg2rad(lng2 - lng1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLng / 2) *
+      Math.sin(dLng / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var distance = R * c;
+
+  return distance;
+}
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+function showHide() {
+  var x = document.getElementById("map");
+  var tools = document.getElementById("tools");
+  var show = document.getElementById("show");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    tools.style.display = "flex";
+    show.style.display = "none";
+  } else {
+    x.style.display = "none";
+    tools.style.display = "none";
+    show.style.display = "block";
+  }
+}
+function defaultSetting() {
+  var tools = document.getElementById("tools");
+  var show = document.getElementById("show");
+  show.style.display = "none";
+  ui.style.display = "none";
+  tools.style.display = "flex";
+  guess.style.height = "40vh";
+  guess.style.width = "35vw";
+  guessbtn.style.display = "block";
+  guessCont.style.display = "none";
+  guess.style.margin = "3.5%";
+  overlay.style.display = "flex";
+  completeDiv.style.display = "none";
+}
+function playagain() {
+  distanceScore = 5000;
+  next.value = "Next";
+  next.onclick = function () {
+    showHide();
+  };
+  played = false;
+  defaultSetting();
+  initMap();
+}
+played = false;
+
+function initMap(data, status) {
+  var n = Math.floor(Math.random() * 9);
+  var ph = phMap(n);
+  currentCordsLat = ph.lat;
+  currentCordsLng = ph.lng;
+  overlay.style.display = "none";
+  ui.style.display = "flex";
+  console.clear();
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("street-view"),
+    {
+      position: ph,
+      pov: {
+        heading: 0,
+        pitch: 0,
+        zoom: 0,
+      },
+      showRoadLabels: false,
+    }
+  );
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 2,
+    maxZoom: 20,
+    minZoom: 2,
+    center: { lat: 0, lng: 0 },
+
+    streetViewControl: false,
+    zoomControl: false,
+    mapTypeControl: false,
+  });
+  map.setOptions({ draggableCursor: "crosshair" });
+  const myinfowindow = new google.maps.InfoWindow({});
+  var marker = new google.maps.Marker({
+    map,
+  });
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
+  });
+  myinfowindow.open(map);
+  // YOUR GUESS MARKER
+  var set = true;
+  map.addListener("click", (mapsMouseEvent) => {
+    guessCont.style.display = "flex";
+    if (played === true) {
+      return;
+    }
+    if (set == true) {
+      guessLatLng = new google.maps.LatLng(
+        mapsMouseEvent.latLng.lat(),
+        mapsMouseEvent.latLng.lng()
+      );
+      guessMarker = new google.maps.Marker({
+        position: mapsMouseEvent.latLng,
+      });
+      guessMarker.setMap(map);
+      set = false;
+    } else {
+      guessMarker.setMap(null);
+      set = true;
+    }
+  });
+
+  // TRUE LOCATION
+  guessbtn.addEventListener("click", () => {
+    targetLatLng = ph;
+    map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 3,
+      maxZoom: 10,
+      minZoom: 2,
+      center: targetLatLng,
+      streetViewControl: false,
+      zoomControl: false,
+      mapTypeControl: false,
+    });
+    targetMarker = new google.maps.Marker({
+      position: targetLatLng,
+      title: "True Location",
+      draggable: false,
+      icon: {
+        url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+      },
+      anchor: new google.maps.Point(15, 15),
+    });
+    marker = new google.maps.Marker({
+      position: guessLatLng,
+      map,
+    });
+    targetMarker.setMap(map);
+    distance = checkDistance();
+    var score = (document.getElementById("score").innerHTML =
+      Math.floor(distance) + "km");
+
+    // playerScore += (5000 * 0.99866017) ^ ((10 * distance) / 14916.862);
+
+    // var playerScoreHTML = (document.getElementById("playerScore").innerHTML =
+    //   playerScore + "");
+    lineCoordinates = [targetLatLng, guessLatLng];
+    linePath = new google.maps.Polyline({
+      path: lineCoordinates,
+      geodesic: true,
+      strokeColor: "#FF0000",
+      strokeOpacity: 1.0,
+      strokeWeight: 2,
+    });
+
+    levelComplete();
+    linePath.setMap(map);
+
+    played = true;
+    next.value = "Next";
+
+    next.onclick = function () {
+      playagain();
+    };
+  });
+}
--- src/js/play.js
+++ src/js/play.js
@@ -1,12 +1,23 @@
 var game = document.getElementById("game");
 var menu = document.getElementById("menu");
 var play = document.getElementById("play");
+var menu_cat = document.getElementById("game-mode");
+var init_menu = document.getElementById("init-menu");
 var playBtn = document.getElementById("play");
 var geoGuessr = document.getElementById("geoGuessr");
+var phBtn = document.getElementById("phBtn");
+var worldBtn = document.getElementById("worldBtn");
 audioBtn = document.getElementById("audio-button");
 audioCollect = document.getElementById("audio-collect");
 audios = document.querySelectorAll("audio");
-function playGame() {
+function clickPlay() {
+  menu_cat.style.display = "flex";
+
+  menu_cat.style.transform = "translateY(0px)";
+  init_menu.style.transition = "1s ease";
+  init_menu.style.display = "none";
+}
+function world() {
   var googleAPI;
   var getLoc;
   var mechanics;
@@ -19,7 +30,7 @@ function playGame() {
   document.documentElement.lastChild.appendChild(googleAPI);
 
   getLoc = document.createElement("script");
-  getLoc.src = "./src/js/getLoc.js";
+  getLoc.src = "./src/js/maps/getLoc.js";
   document.documentElement.lastChild.appendChild(getLoc);
 
   mechanics = document.createElement("script");
@@ -27,7 +38,27 @@ function playGame() {
 
   document.documentElement.firstChild.appendChild(mechanics);
 }
+function ph() {
+  var googleAPI;
+  var getLoc;
+  var mechanics;
+  menu.style.display = "none";
+  game.style.display = "block";
+  googleAPI = document.createElement("script");
+  googleAPI.src = "./src/js/mapsJavaScriptAPI.js";
+  googleAPI.async = true;
+  googleAPI.defer = true;
+  document.documentElement.lastChild.appendChild(googleAPI);
 
+  getLoc = document.createElement("script");
+  getLoc.src = "./src/js/maps/phLandmarks.js";
+  document.documentElement.lastChild.appendChild(getLoc);
+
+  mechanics = document.createElement("script");
+  mechanics.src = "./src/js/hide.js";
+
+  document.documentElement.firstChild.appendChild(mechanics);
+}
 playBtn.addEventListener(
   "mouseover",
   function () {
@@ -66,6 +97,64 @@ geoGuessr.addEventListener(
   function () {
     audioBtn.pause();
     audioBtn.currentTime = 0;
+  },
+  false
+);
+geoGuessr.addEventListener(
+  "click",
+  function () {
+    audioCollect.play();
+  },
+  false
+);
+
+// MENU CATEGORY AUDIO
+
+phBtn.addEventListener(
+  "mouseover",
+  function () {
+    audioBtn.play();
+  },
+  false
+);
+
+phBtn.addEventListener(
+  "mouseleave",
+  function () {
+    audioBtn.pause();
+    audioBtn.currentTime = 0;
+  },
+  false
+);
+
+phBtn.addEventListener(
+  "click",
+  function () {
+    audioCollect.play();
+  },
+  false
+);
+
+worldBtn.addEventListener(
+  "mouseover",
+  function () {
+    audioBtn.play();
+  },
+  false
+);
+
+worldBtn.addEventListener(
+  "mouseleave",
+  function () {
+    audioBtn.pause();
+    audioBtn.currentTime = 0;
+  },
+  false
+);
+worldBtn.addEventListener(
+  "click",
+  function () {
+    audioCollect.play();
   },
   false
 );
