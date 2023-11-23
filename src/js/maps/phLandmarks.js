b642c27c06a8b86615db95a4626ab359377a8c06
Jizan Diaz Villafuerte <diazjizan@gmail.com>
2023-11-23 20:07:41 +0800

    Added count animation

--- src/js/maps/phLandmarks.js
+++ src/js/maps/phLandmarks.js
@@ -13,6 +13,9 @@ var distanceScore = 5000;
 var currentCordsLat, currentCordsLng;
 var currentLoc, currentCoords;
 var round = 1;
+// AUDIOS
+var tally = document.getElementById("audio-tally");
+//
 document.getElementById("mapname").innerHTML = "Philippines";
 function phMap(n) {
   var location = [
@@ -227,7 +230,14 @@ function initMap(data, status) {
 
         levelComplete();
         linePath.setMap(map);
-        document.getElementById("playerScore").innerHTML = "" + points;
+        var counter = points - 50;
+        if (points > 100) {
+          animateValue("playerScore", counter, points, 2800);
+        } else {
+          counter = points;
+          animateValue("playerScore", counter, points, 2800);
+        }
+        tally.play();
         played = true;
         next.value = "Next";
         next.onclick = function () {
@@ -240,4 +250,19 @@ function initMap(data, status) {
   } else {
     window.location.reload();
   }
+}
+function animateValue(id, start, end, duration) {
+  if (start === end) return;
+  var range = end - start;
+  var current = start;
+  var increment = end > start ? 1 : -1;
+  var stepTime = Math.abs(Math.floor(duration / range));
+  var obj = document.getElementById(id);
+  var timer = setInterval(function () {
+    current += increment;
+    obj.innerHTML = current;
+    if (current == end) {
+      clearInterval(timer);
+    }
+  }, stepTime);
 }
--- src/js/maps/randomWorld.js
+++ src/js/maps/randomWorld.js
@@ -11,6 +11,9 @@ var tools = document.getElementById("tools");
 var playerScore = 0;
 var distanceScore = 5000;
 var round = 1;
+// AUDIOS
+var tally = document.getElementById("audio-tally");
+//
 document.getElementById("mapname").innerHTML = "World";
 
 var currentCordsLat, currentCordsLng;
@@ -219,9 +222,17 @@ function initMap(data, status) {
 
         levelComplete();
         linePath.setMap(map);
-        document.getElementById("playerScore").innerHTML = "" + points;
         played = true;
         next.value = "Next";
+        var counter = points - 50;
+        if (points > 100) {
+          animateValue("playerScore", counter, points, 2800);
+        } else {
+          counter = points;
+          animateValue("playerScore", counter, points, 2800);
+        }
+        tally.play();
+        // document.getElementById("playerScore").innerHTML = "" + counter;
 
         next.onclick = function () {
           round++;
@@ -233,4 +244,19 @@ function initMap(data, status) {
   } else {
     window.location.reload();
   }
+}
+function animateValue(id, start, end, duration) {
+  if (start === end) return;
+  var range = end - start;
+  var current = start;
+  var increment = end > start ? 1 : -1;
+  var stepTime = Math.abs(Math.floor(duration / range));
+  var obj = document.getElementById(id);
+  var timer = setInterval(function () {
+    current += increment;
+    obj.innerHTML = current;
+    if (current == end) {
+      clearInterval(timer);
+    }
+  }, stepTime);
 }
